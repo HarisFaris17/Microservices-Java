9cc4800e08b2f8bdd766534ded6b56cb8aeab284
HarisFaris17 <harisfaris17@gmail.com>
2022-12-31 17:39:50 +0700

    add customer service to handle http request and customer repository to handle database transactions

--- customer/src/main/java/com/haris/app/CustomerService.java
+++ customer/src/main/java/com/haris/app/CustomerService.java
@@ -1,11 +1,15 @@
 package com.haris.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
-    public CustomerService(){
 
+    private final CustomerRepository customerRepository;
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
     }
     public void customerRegistration(CustomerRequest customerRequest){
         Customer customer = Customer.builder()
@@ -13,6 +17,7 @@ public class CustomerService {
                                         .lastName(customerRequest.lastName())
                                         .email(customerRequest.email())
                                         .build();
+                                        
         // @ TODO: regist firstName
         // @ TODO: regist lastName
         // @ TODO: regist email
